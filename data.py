f541156cc46725553a97c2f44d8b0feacdd9c212
Dimon4ik5432 <dimon4ik.5432@gmail.com>
2025-02-08 18:47:27 +0200

    first

---
+++ data.py
@@ -0,0 +1,54 @@
+data = [
+    {
+        'question': 'Як перекласти яблуко',
+        'right_answer': 'apple',
+        'wrong_answers': ['caterpillar', 'application', 'building']
+    },
+    {
+        'question': 'Як перекласти собака',
+        'right_answer': 'dog',
+        'wrong_answers': ['cat', 'carrot', 'hotdog']
+    },
+    {
+        'question': 'Як перекласти книга',
+        'right_answer': 'book',
+        'wrong_answers': ['notebook', 'magazine', 'pen']
+    },
+    {
+        'question': 'Як перекласти автомобіль',
+        'right_answer': 'car',
+        'wrong_answers': ['train', 'bicycle', 'plane']
+    },
+    {
+        'question': 'Як перекласти сонце',
+        'right_answer': 'sun',
+        'wrong_answers': ['moon', 'star', 'sky']
+    },
+    {
+        'question': 'Як перекласти вода',
+        'right_answer': 'water',
+        'wrong_answers': ['fire', 'ice', 'wind']
+    },
+    {
+        'question': 'Як перекласти школа',
+        'right_answer': 'school',
+        'wrong_answers': ['university', 'library', 'office']
+    },
+    {
+        'question': 'Як перекласти олівець',
+        'right_answer': 'pencil',
+        'wrong_answers': ['pen', 'eraser', 'ruler']
+    },
+    {
+        'question': 'Як перекласти стіл',
+        'right_answer': 'table',
+        'wrong_answers': ['chair', 'bed', 'sofa']
+    },
+    {
+        'question': 'Як перекласти двері',
+        'right_answer': 'door',
+        'wrong_answers': ['window', 'wall', 'roof']
+    }
+]
+
+number_data = 0
